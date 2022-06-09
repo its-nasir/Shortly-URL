c939af4f810769f38097d21d9184d3284d286612
its-nasir <nasirmoh8802@gmail.com>
2022-06-09 15:45:03 +0530

    minor change

--- js/script.js
+++ js/script.js
@@ -2,35 +2,51 @@
 console.log("i'm here");
 
 const hidden_div = document.querySelector(".hidden_div");
-const link_input = document.querySelector(".srch-2");
+const link_input = document.getElementById("search_bar");
 const submit = document.querySelector(".submit");
-
-
+let urlD = document.querySelector(".url_hdn");
 let alldataS = [];
+
 var dataL = JSON.parse(localStorage.getItem("data"));
-            alldataS.push(dataL);
-            console.log(alldataS);
-  hidden_div.innerHTML = ` <div class="shadow p-3 mb-5 bg-body rounded  load  extra-hidden mt-3 bg-light d-flex ">
-            ${dataL.url}
-            <a class="dAtag" target="_blank"  href="${dataL.S_url}" class="js_tag">${dataL.S_url}</a>
-            <button>Copy</button>
-            </div>`;
+if(dataL && dataL.length>=1){
+  alldataS = [...dataL];
+    let elemDiv= '';
+    alldataS.forEach(elementt => {
+      elemDiv += ` <div class="shadow p-3 mb-5 bg-body rounded  load  extra-hidden mt-3 bg-light d-flex ">
+      ${elementt.url}
+      <a target="_blank" href="${elementt.S_url}" class="js_tag">${elementt.S_url}</a>
+      <button>Copy</button>
+      </div>`;
+    });
+    hidden_div.innerHTML= elemDiv;
+}
 
 submit.addEventListener("click", (event) => {
-  event.preventDefault();
+  // console.log(link_input.value.length);
+  if(link_input.value.length >=1){
+    console.log("working.....");
+    urlD.classList.add('url_1')
 
+
+  // event.preventDefault();
+  let store ={};
   let APi = fetch(`https://api.shrtco.de/v2/shorten?url=${link_input.value}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      hidden_div.innerHTML += ` <div class="shadow p-3 mb-5 bg-body rounded  load  extra-hidden mt-3 bg-light d-flex ">
+  .then((res) => res.json())
+  .then((data) => {
+    hidden_div.innerHTML += ` <div class="shadow p-3 mb-5 bg-body rounded  load  extra-hidden mt-3 bg-light d-flex ">
     ${link_input.value}
     <a href="${data.result.full_short_link}" class="js_tag">${data.result.full_short_link}</a>
     <button>Copy</button>
     </div>`;
-      localStorage.setItem("data", JSON.stringify({'S_url':data.result.full_short_link,'url':link_input.value}));
+    store['S_url'] = data.result.full_short_link;
+    store['url'] = link_input.value;
+    alldataS.push(store);
+    localStorage.setItem("data", JSON.stringify(alldataS));
+  });
+  console.log(APi,"api calling");
+}else{
+  urlD.classList.remove('url_1')
 
-    });
 
-  console.log(APi);
+}
 });
